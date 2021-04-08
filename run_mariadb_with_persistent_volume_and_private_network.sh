16b49b58774c78732df316f37056752c41018e5c
Ajin NF <nurulfajrin1105@gmail.com>
2021-04-08 10:49:21 +0800

    adding grade 2

---
+++ run_mariadb_with_persistent_volume_and_private_network.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker container run -d \
+--name wordpressdb \
+-e MYSQL_ROOT_PASSWORD='redhat' \
+-e MYSQL_DATABASE='wordpress' \
+-e MYSQL_USER='wordpress' \
+-e MYSQL_PASSWORD='redhat' \
+-v /root/var/lib/mysql:/var/lib/mysql \
+--network mariadb-wp-privnet \
+mariadb
